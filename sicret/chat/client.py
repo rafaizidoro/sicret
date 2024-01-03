7e93ed0fe84ca17f27742d113867c0483b6f80a0
Rafael Izidoro <izidoro.rafa@gmail.com>
2024-01-03 12:37:45 -0300

    wip

--- sicret/chat/client.py
+++ sicret/chat/client.py
@@ -35,36 +35,3 @@ async def start(host, port):
 
 def run_client(host, port):
     asyncio.run(start(host, port))
-
-
-# import asyncio
-
-# import socketio
-# from aioconsole import ainput
-
-# sio = socketio.AsyncClient()
-
-# @sio.event
-# async def connect():
-#     print("Connected to server")
-#     asyncio.create_task(send_messages())
-
-# @sio.event
-# async def message(data):
-#     print(data)
-
-# @sio.event
-# async def disconnect():
-#     print("Disconnected from server")
-
-# async def send_messages():
-#     while True:
-#         message = await ainput("Your message: ")
-#         await sio.send(message)
-
-# async def main():
-#     await sio.connect('http://127.0.0.1:6969')
-#     await sio.wait()
-
-# if __name__ == '__main__':
-#     asyncio.run(main())
